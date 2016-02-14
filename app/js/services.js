ddc5cb08a26ed27539f262970a51e3037fedcb2e
Fotis Evangelou <fevangelou@users.noreply.github.com>
2016-02-14 21:55:36 +0200

    Updated version check

--- app/js/services.js
+++ app/js/services.js
@@ -48,14 +48,14 @@ var CURRENT_VERSION = '1.6.0';
 if(versionCompare(CURRENT_VERSION, ENGINTRON_VERSION)){
 	var b = document.getElementsByTagName('body')[0];
 	var notice = document.createElement("div");
-	notice.setAttribute('style', 'font-size:14px;text-align:center;padding:10px;background:#00b243;color:#fff;opacity:0.9;position:absolute;top:0;left:0;right:0;border:2px solid #008d23;');
-	notice.innerHTML = '<div style="">A newer version of Engintron (v'+CURRENT_VERSION+') is now available.<br /><br />Make sure you backup any custom settings and ';
+	notice.setAttribute('style', 'font-size:12px;text-align:center;padding:10px;background:#00b243;color:#fff;opacity:0.9;position:absolute;top:0;left:0;right:0;border:2px solid #008d23;');
+	notice.innerHTML = '<div style="">A newer version of Engintron (v'+CURRENT_VERSION+') is now available. Make sure you backup any custom settings and ';
 	if(versionCompare(ENGINTRON_VERSION, '1.5.2')){
 		notice.innerHTML += '<a style="color:#fff;font-weight:bold;" href="engintron.php?op=engintron_update">click here to update to the latest version</a>.';
 	} else {
 		notice.innerHTML += 'update Engintron from the command-line as root user.';
 	}
-	notice.innerHTML += '<br /><br />For more info check the <a style="color:#fff;font-weight:bold;" target="_blank" href="https://github.com/engintron/engintron#changelog">Engintron release changelog</a>.</div>';
+	notice.innerHTML += '<br />For more info check the <a style="color:#fff;font-weight:bold;" target="_blank" href="https://github.com/engintron/engintron#changelog">Engintron release changelog</a>.</div>';
 	window.onload = function(){
 		b.appendChild(notice);
 	}
