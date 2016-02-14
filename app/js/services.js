bfc8d395fcc287ebd780ed4eb0b2bd09a4f9f272
Fotis Evangelou <fevangelou@users.noreply.github.com>
2016-02-14 22:08:04 +0200

    Updated version check

--- app/js/services.js
+++ app/js/services.js
@@ -45,18 +45,15 @@ versionCompare = function(left, right) {
 
 var CURRENT_VERSION = '1.6.0';
 
-if(versionCompare(CURRENT_VERSION, ENGINTRON_VERSION)){
-	var b = document.getElementsByTagName('body')[0];
-	var notice = document.createElement("div");
-	notice.setAttribute('style', 'font-size:12px;text-align:center;padding:10px;background:#00b243;color:#fff;opacity:0.9;position:absolute;top:0;left:0;right:0;border-bottom:2px solid #008d23;');
-	notice.innerHTML = '<div style="width:92%;margin:auto;padding:0;">A newer version of Engintron (v'+CURRENT_VERSION+') is now available. Make sure you backup any custom settings and ';
-	if(versionCompare(ENGINTRON_VERSION, '1.5.2')){
-		notice.innerHTML += '<a style="color:#fff;font-weight:bold;" href="engintron.php?op=engintron_update">click here to update to the latest version</a>.';
-	} else {
-		notice.innerHTML += 'update Engintron from the command-line as root user.';
-	}
-	notice.innerHTML += '<br />For more info check the <a style="color:#fff;font-weight:bold;" target="_blank" href="https://github.com/engintron/engintron#changelog">Engintron release changelog</a>.</div>';
-	window.onload = function(){
-		b.appendChild(notice);
+window.onload = function(){
+	if(versionCompare(CURRENT_VERSION, ENGINTRON_VERSION)){
+		var b = document.getElementsByTagName('body')[0];
+		if(versionCompare(ENGINTRON_VERSION, '1.5.2')){
+			var noticeAction = '<a style="color:#fff;font-weight:bold;" href="engintron.php?op=engintron_update">click here to update to the latest version</a>';
+		} else {
+			var noticeAction = 'update Engintron from the command-line as root user';
+		}
+		var notice = '<div style="font-size:12px;text-align:center;padding:10px;background:#00b243;color:#fff;opacity:0.9;position:absolute;top:0;left:0;right:0;border-bottom:2px solid #008d23;"><div style="width:92%;margin:auto;padding:0;">A newer version of Engintron (v'+CURRENT_VERSION+') is now available. Make sure you backup any custom settings and '+noticeAction+'.<br />For more info check the <a style="color:#fff;font-weight:bold;" target="_blank" href="https://github.com/engintron/engintron#changelog">Engintron release changelog</a>.</div></div>';
+		b.innerHTML += notice;
 	}
 }
