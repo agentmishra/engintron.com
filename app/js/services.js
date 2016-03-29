89186806fce969073b6e5126866d0a9620bfcd08
Fotis Evangelou <fevangelou@users.noreply.github.com>
2016-03-30 00:47:06 +0300

    Minor bug fix in update system

--- app/js/services.js
+++ app/js/services.js
@@ -47,13 +47,8 @@
 	var CURRENT_VERSION = '1.6.1';
 	var b = document.getElementsByTagName('body')[0];
 
-	if(versionCompare(CURRENT_VERSION, ENGINTRON_VERSION)){
-		if(versionCompare(ENGINTRON_VERSION, '1.5.1')){
-			var noticeAction = '<a style="color:#fff;font-weight:bold;" href="engintron.php?op=engintron_update">click here to update to the latest version</a>';
-		} else {
-			var noticeAction = 'update Engintron from the command-line as root user';
-		}
-		var notice = '<div style="font-size:12px;text-align:center;padding:10px;background:#00b243;color:#fff;opacity:0.9;position:absolute;top:0;left:0;right:0;border-bottom:2px solid #008d23;z-index:999999;"><div style="width:92%;margin:auto;padding:0;">A newer version of Engintron (v'+CURRENT_VERSION+') is now available. Make sure you backup any custom rules in Nginx and '+noticeAction+'.<br />For more info check the <a style="color:#fff;font-weight:bold;" target="_blank" href="https://github.com/engintron/engintron#changelog">Engintron release changelog</a>.</div></div>';
+	if(versionCompare(CURRENT_VERSION, ENGINTRON_VERSION)>0){
+		var notice = '<div style="font-size:12px;text-align:center;padding:10px;background:#00b243;color:#fff;opacity:0.9;position:absolute;top:0;left:0;right:0;border-bottom:2px solid #008d23;z-index:999999;"><div style="width:92%;margin:auto;padding:0;">A newer version of Engintron (v'+CURRENT_VERSION+') is now available. Make sure you backup any custom rules in Nginx and <a style="color:#fff;font-weight:bold;" href="engintron.php?op=engintron_update">click here to update to the latest version</a>.<br />For more info check the <a style="color:#fff;font-weight:bold;" target="_blank" href="https://github.com/engintron/engintron#changelog">Engintron release changelog</a>.</div></div>';
 		var mountNotice = document.createElement('div');
 		mountNotice.innerHTML = notice;
 		b.appendChild(mountNotice);
