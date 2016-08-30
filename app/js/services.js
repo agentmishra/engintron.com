f963bb2d401ef6951ab62c7f16f2d85d278537c0
Fotis Evangelou <fevangelou@users.noreply.github.com>
2016-08-30 09:04:28 +0300

    Update references to latest version

--- app/js/services.js
+++ app/js/services.js
@@ -44,7 +44,7 @@
 	}
 
 	/* === Engintron App === */
-	var CURRENT_VERSION = '1.7.1';
+	var CURRENT_VERSION = '1.7.2';
 	var b = document.getElementsByTagName('body')[0];
 
 	if(versionCompare(CURRENT_VERSION, ENGINTRON_VERSION)>0){
