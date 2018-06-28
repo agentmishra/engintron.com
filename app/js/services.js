6506880a902562aa9d161f2085bab2849104874a
Fotis Evangelou <fevangelou@users.noreply.github.com>
2018-06-29 02:51:53 +0300

    Pushed v1.8.13

--- app/js/services.js
+++ app/js/services.js
@@ -44,7 +44,7 @@
 	}
 
 	/* === Engintron App === */
-	var CURRENT_VERSION = '1.8.12';
+	var CURRENT_VERSION = '1.8.13';
 	var b = document.getElementsByTagName('body')[0];
 
 	if(versionCompare(CURRENT_VERSION, ENGINTRON_VERSION)>0){
